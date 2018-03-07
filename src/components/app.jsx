434787dccfcb503ea97890f3e21431bff776f8db
DanielJBailey <Dbailey0210@gmail.com>
2018-03-06 20:32:08 -0600

    finish first lab for react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -7,14 +7,15 @@ import './styles.css';
 //     return element;
 // }
 
+let load = 'Loading . . .';
+let place = 'Type your name';
+
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             text: 'This is my first lab for Covalence',
-            place: 'Type your name:',
-            load: 'Page Loading...',
             name: '',
             hasLoaded: false
         }
@@ -32,6 +33,11 @@ class App extends Component {
         this.setState({ name: value })
     }
 
+    // componentDidMount = () => {
+    //     this.setState({ hasLoaded: true})
+    // }
+    
+
     render() {
 
         if (this.state.hasLoaded === true) {
@@ -42,7 +48,7 @@ class App extends Component {
                     <h1 className='header-text'> {this.props.string} </h1>
                     <h1 className='header-text'> {this.state.text} </h1>
                     <input
-                        placeholder={this.state.place}
+                        placeholder={ place }
                         className='input-box'
                         value={this.state.name}
                         onChange={(event) => this.handleInputChange(event.target.value)}
@@ -57,7 +63,7 @@ class App extends Component {
         } else {
             return (
                 <React.Fragment>
-                    <h1 className='header-text'> {this.state.load} </h1>
+                    <h1 className='header-text'> { load } </h1>
                     <button
                         className='btn btn-lg button ml-4 mt-4'
                         onClick={this.handleClick}
